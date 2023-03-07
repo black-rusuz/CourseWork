c8244431bbf7cc39d683b92b9a3e1d2b493c2d42
RUzai <RUzairov@gmail.com>
2023-03-07 16:47:06 +0300

    test

--- src/main/java/ru/sfedu/trainpick/utils/TestData.java
+++ src/main/java/ru/sfedu/trainpick/utils/TestData.java
@@ -1,4 +1,20 @@
 package ru.sfedu.trainpick.utils;
 
+import ru.sfedu.trainpick.model.bean.*;
+
 public class TestData {
+    public final ChildPassenger cp1 = new ChildPassenger(11, "Dyadka Valentin Viktorovich", "21.01.2019", "6022 989898", false);
+    public final ChildPassenger cp2 = new ChildPassenger(12, "Pushnoy Fedor Ivanovich", "14.02.2022", "6022 121212", true);
+
+    public final DiscountPassenger dp1 = new DiscountPassenger(21, "Sechenov Dmitriy Petrovich", "12.01.1969", "6019 324234", 0.1, "Pensioner");
+    public final DiscountPassenger dp2 = new DiscountPassenger(22, "Mendeleev Anton Pavlovich", "26.02.1985", "6012 445345", 0.5, "Soldier");
+
+    public final Passenger ps1 = new Passenger(31, "Volya Pavel Igorevich", "16.04.2000", "6014 566556");
+    public final Passenger ps2 = new Passenger(32, "Poslov Semen Daskovich", "11.07.1992", "6012 878778");
+
+    public final Train tr1 = new Train(41, "Moscow", "Rostov-on-Don", "21.03.2022 00:00", "21.03.2022 19:00", 3500);
+    public final Train tr2 = new Train(42, "Adler", "Ekaterinburg", "22.03.2022 00:00", "23.03.2022 19:00", 6000);
+
+    public final Ticket ti1 = new Ticket(51, ps1, tr1, "", 3500, false);
+    public final Ticket ti2 = new Ticket(52, dp2, tr2, "", 3000, true);
 }
--- src/test/java/ru/sfedu/trainpick/api/ApiTest.java
+++ src/test/java/ru/sfedu/trainpick/api/ApiTest.java
@@ -1,57 +1,50 @@
 package ru.sfedu.trainpick.api;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.sfedu.trainpick.utils.Constants;
 import ru.sfedu.trainpick.utils.TestData;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 public abstract class ApiTest extends TestData {
     protected AbstractDataProvider dp;
 
     @Test
     void test() {
-        dp = new DataProviderXml();
     }
 
     @BeforeEach
     void setUp() {
-//        dp.insertBodyPart(b1);
-//        dp.insertBodyPart(b2);
-//
-//        dp.insertElectricPart(e1);
-//        dp.insertElectricPart(e2);
-//
-//        dp.insertRunningPart(r1);
-//        dp.insertRunningPart(r2);
-//
-//        dp.insertUser(u1);
-//        dp.insertUser(u2);
-//
-//        dp.insertOrder(o1);
-//        dp.insertOrder(o2);
+        dp.insertChildPassenger(cp1);
+        dp.insertChildPassenger(cp2);
+
+        dp.insertDiscountPassenger(dp1);
+        dp.insertDiscountPassenger(dp2);
+
+        dp.insertPassenger(ps1);
+        dp.insertPassenger(ps2);
+
+        dp.insertTrain(tr1);
+        dp.insertTrain(tr2);
+
+        dp.insertTicket(ti1);
+        dp.insertTicket(ti2);
     }
 
     @AfterEach
     void tearDown() {
-//        dp.deleteBodyPart(b1.getId());
-//        dp.deleteBodyPart(b2.getId());
-//
-//        dp.deleteElectricPart(e1.getId());
-//        dp.deleteElectricPart(e2.getId());
-//
-//        dp.deleteRunningPart(r1.getId());
-//        dp.deleteRunningPart(r2.getId());
-//
-//        dp.deleteUser(u1.getId());
-//        dp.deleteUser(u2.getId());
-//
-//        dp.deleteOrder(o1.getId());
-//        dp.deleteOrder(o2.getId());
+        dp.deleteChildPassenger(cp1.getId());
+        dp.deleteChildPassenger(cp2.getId());
+
+        dp.deleteDiscountPassenger(dp1.getId());
+        dp.deleteDiscountPassenger(dp2.getId());
+
+        dp.deletePassenger(ps1.getId());
+        dp.deletePassenger(ps2.getId());
+
+        dp.deleteTrain(tr1.getId());
+        dp.deleteTrain(tr2.getId());
+
+        dp.deleteTicket(ti1.getId());
+        dp.deleteTicket(ti2.getId());
     }
 }
