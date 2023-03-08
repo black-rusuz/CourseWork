553623daa35da685d46388b0d1323bd610962637
RUzai <RUzairov@gmail.com>
2023-03-08 15:14:20 +0300

    tests

--- src/test/java/ru/sfedu/trainpick/api/ApiTest.java
+++ src/test/java/ru/sfedu/trainpick/api/ApiTest.java
@@ -1,21 +1,104 @@
 package ru.sfedu.trainpick.api;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ru.sfedu.trainpick.model.bean.Passenger;
+import ru.sfedu.trainpick.model.bean.Ticket;
+import ru.sfedu.trainpick.model.bean.Train;
 import ru.sfedu.trainpick.utils.TestData;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public abstract class ApiTest extends TestData {
     protected AbstractDataProvider dp;
 
     @Test
-    void test() {
-        dp.addPassenger(tr1.getFrom(), tr1.getTo(), cp1.getId(), false);
-        dp.findTrain(tr1.getFrom(), tr1.getTo());
-        dp.calculateDuration(tr2.getDeparture(), tr2.getArrival());
-        dp.calculatePrice(tr2.getPrice(), 0.5);
-        dp.viewPassengers(tr1.getId(), 0);
-        dp.payTicket(ti1.getId());
+    void addPassengerPos() {
+        Optional<Ticket> actualTicket = dp.addPassenger(tr1.getFrom(), tr1.getTo(), cp1.getId(), false);
+        Optional<Ticket> expectedTicket = Optional.of(new Ticket(actualTicket.get().getId(), cp1, tr1, "0 day(s), 19 hours", ti1.getCost(), false));
+        Assertions.assertEquals(expectedTicket, actualTicket);
+        dp.deleteTicket(actualTicket.get().getId());
+    }
+
+    @Test
+    void addPassengerNeg() {
+        Optional<Ticket> ticket = dp.addPassenger("", "", 0, false);
+        Ticket actualTicket = ticket.orElse(null);
+        Assertions.assertNull(actualTicket);
+    }
+
+    @Test
+    void findTrainPos() {
+        Optional<Train> actualTrain = dp.findTrain(tr1.getFrom(), tr1.getTo());
+        Optional<Train> expectedTrain = Optional.of(tr1);
+        Assertions.assertEquals(expectedTrain, actualTrain);
+    }
+
+    @Test
+    void findTrainNeg() {
+        Optional<Train> train = dp.findTrain("", "");
+        Train actualTrain = train.orElse(null);
+        Assertions.assertNull(actualTrain);
+    }
+
+    @Test
+    void calculateDurationPos() {
+        String expectedDuration = "1 day(s), 19 hours";
+        String actualDuration = dp.calculateDuration(tr2.getDeparture(), tr2.getArrival());
+        Assertions.assertEquals(expectedDuration, actualDuration);
+    }
+
+    @Test
+    void calculateDurationNeg() {
+        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> dp.calculateDuration("", ""));
+    }
+
+    @Test
+    void calculatePricePos() {
+        double expectedPrice = tr1.getPrice();
+        double actualPrice = dp.calculatePrice(tr1.getPrice(), 0);
+        Assertions.assertEquals(expectedPrice, actualPrice);
+    }
+
+    @Test
+    void calculatePriceNeg() {
+        double unexpectedPrice = tr1.getPrice();
+        double actualPrice = dp.calculatePrice(tr1.getPrice(), 0.5);
+        Assertions.assertNotEquals(unexpectedPrice, actualPrice);
+    }
+
+    @Test
+    void viewPassengersPos() {
+        List<Passenger> expectedPassengers = List.of(ps1, dp2);
+        List<Passenger> actualPassengers = dp.viewPassengers(tr1.getId(), 0);
+        Assertions.assertEquals(expectedPassengers, actualPassengers);
+    }
+
+    @Test
+    void viewPassengersNeg() {
+        List<Passenger> expectedPassengers = new ArrayList<>();
+        List<Passenger> actualPassengers = dp.viewPassengers(tr2.getId(), 0);
+        Assertions.assertEquals(expectedPassengers, actualPassengers);
+    }
+
+    @Test
+    void payTicketPos() {
+        Ticket expectedTicket = ti1;
+        expectedTicket.setPaid(true);
+        Optional<Ticket> ticket = dp.payTicket(ti1.getId());
+        Ticket actualTicket = ticket.orElse(null);
+        Assertions.assertEquals(expectedTicket, actualTicket);
+    }
+
+    @Test
+    void payTicketNeg() {
+        Optional<Ticket> ticket = dp.payTicket(0);
+        Ticket actualTicket = ticket.orElse(null);
+        Assertions.assertNull(actualTicket);
     }
 
     @BeforeEach
